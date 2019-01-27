0fb1b65ecfcead4b01a9ddcd4d8365f022e4f58a
daeyonglee <dleo238@gmail.com>
2019-01-27 22:37:25 +0900

    add signup form basic

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,28 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
+'''
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
+'''
+
+signup = CreateView.as_view(model=User,
+                            form_class=UserCreationForm,
+                            success_url=settings.LOGIN_URL,
+                            template_name='accounts/signup.html')
 
 @login_required
 def profile(request):
